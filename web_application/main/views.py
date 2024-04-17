97aa0ccd0f6b6aa8057f066e54e42d2203ade3e9
Neko-Nika <veronicachastova13@gmail.com>
2024-04-17 20:17:16 +0300

    Add navbar, add signout

--- web_application/main/views.py
+++ web_application/main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
 @login_required(login_url="/login")
 def index(request):
-    return render(request, 'main.html')
+    return render(request, 'instructions.html')
 
 
 def login(request):
@@ -13,3 +14,9 @@ def login(request):
 
 def register(request):
     return render(request, "register.html")
+
+
+@login_required(login_url="/login")
+def signout(request):
+    logout(request)
+    return redirect("login")
